98fb2b951451159120bf1d9f2a4dcf9c18889d76
Minoo <minoo.hadi2904@gmail.com>
2023-03-03 18:11:23 +0330

    feat: create routes with product id

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -7,18 +7,21 @@ function GateWay() {
         <BrowserRouter>
             <Routes>
                 <Route element={<MainLayout />}>
-                    <Route path="/" element={<h1>گروه محصولات</h1>} />
-                    <Route path="/subCategories" element={<h1>محصولات هر گروه</h1>} />
-                    <Route path="/product" element={<h1>محصول</h1>} />
+                    {/* صنایع دتی، کتاب و مجله، لوازم التحریر */}
+                    <Route path="/" element={<h1>گروه محصولات</h1>} />  
+                    {/* خاتم و منبت کاری و ...، رمان و ...، ابزار نقاشی و رنگ آمیزی و ... */}
+                    <Route path="/category?id=:catgoryId" element={<h1>زیرگروه محصولات</h1>} />
+                    <Route path="products" element={<h1>محصولات هر زیرگروه</h1>} />
+                    <Route path="/product?id=:productId" element={<h1>محصول</h1>} />
                     <Route path="/card" element={<h1>سبد خرید</h1>} />
                     <Route path="/favorites" element={<h1>محصولات منتخب</h1>} />
                     <Route path="/checkout" element={<h1>پرداخت</h1>} />
                 </Route>
                 <Route path="/adminLogin" element={<LoginLayout />} />
-                <Route element={<AdminLayout />}>
-                    <Route path="/orders" element={<h1>سفارش ها</h1>} />
-                    <Route path="/stocs" element={<h1>موجودی ها</h1>} />
-                    <Route path="/products" element={<h1>محصولات</h1>} />
+                <Route path="/admin" element={<AdminLayout />}>
+                    <Route path="/admin/orders" element={<h1>سفارش ها</h1>} />
+                    <Route path="/admin/stocs" element={<h1>موجودی ها</h1>} />
+                    <Route path="/admin/products" element={<h1>محصولات</h1>} />
                 </Route>
             </Routes>
         </BrowserRouter>

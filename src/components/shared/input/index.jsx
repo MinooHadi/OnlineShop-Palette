b955bf0814f0da6884bc0a8a47b1e1900460d208
Minoo <minoo.hadi2904@gmail.com>
2023-03-06 15:31:01 +0330

    feat: complete inpute component

--- src/components/shared/input/index.jsx
+++ src/components/shared/input/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-function Input() {
-  return <input />;
+function Input(props) {
+  return (
+    <div className="flex flex-col gap-2">
+      <label> {props.lable} </label>
+      <input type={props.type} className={props.className} value={props.value} />
+    </div>
+  );
 }
 
 export default Input;

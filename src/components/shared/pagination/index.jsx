a3223498f870811962b646e872faaf8fd717404a
Minoo <minoo.hadi2904@gmail.com>
2023-03-09 23:46:30 +0330

    refactor: refactore style

--- src/components/shared/pagination/index.jsx
+++ src/components/shared/pagination/index.jsx
@@ -14,7 +14,7 @@ function Pagination(props) {
     <div className="m-auto my-5 flex flex-row-reverse gap-2 justify-center items-center">
       {createPaginationDiv().map((item) => (
         <div
-          className="bg-yellow-600 w-8 h-8 flex justify-center items-center rounded-full text-yellow-100 hover:bg-yellow-100 hover:text-yellow-600 hover:cursor-pointer"
+          className="bg-rose-400 w-8 h-8 flex justify-center items-center rounded-full text-slate-600 vazir-extraBold hover:bg-rose-100  hover:cursor-pointer"
           onClick={props.onClick}
         >
           {item}

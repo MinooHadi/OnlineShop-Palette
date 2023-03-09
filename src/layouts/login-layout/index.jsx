dec992b69bb08772edac02fc768f0673b15ecc89
Minoo <minoo.hadi2904@gmail.com>
2023-03-09 16:53:17 +0330

    feat: handle localstorage

--- src/layouts/login-layout/index.jsx
+++ src/layouts/login-layout/index.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Form } from "../../components/shared";
 
 function LoginLayout() {
-  return <Form />;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    localStorage.getItem("token") && navigate("/admin/orders");
+  }, []);
+
+  return (
+    <>{localStorage.getItem("token") ? undefined : <Form />}</>
+  );
 }
 
 export default LoginLayout;

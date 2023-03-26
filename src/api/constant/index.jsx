e49c1c63a84f637a44dd0054497fd72fe86c3690
Minoo <minoo.hadi2904@gmail.com>
2023-03-26 22:32:07 +0400

    feat: handle refresh token and access token

--- src/api/constant/index.jsx
+++ src/api/constant/index.jsx
@@ -4,6 +4,47 @@ export const baseURL = "http://localhost:3001";
 
 export const instance = axios.create({
   baseURL,
+  timeout: 15000,
 });
 
-export const toFormData = axios.toFormData
+instance.interceptors.request.use(
+  (config) => {
+    const authToken = localStorage.getItem("token");
+    if (authToken !== undefined) {
+      if (!config.url.includes("auth/refresh-token")) {
+        config.headers.token = authToken;
+      }
+      config.timeout = 800000;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const {
+      response: { status },
+    } = error;
+
+    if (status === 401) {
+      instance.defaults.headers.refreshToken =
+        localStorage.getItem("refresh_token");
+      instance.post("/auth/refresh-token").then(({ data }) => {
+        const { accessToken } = data;
+        localStorage.setItem("token", accessToken);
+        error.config.headers.token = accessToken
+        instance(error.config).then(() => window.location.reload());
+      });
+    } else {
+      return Promise.reject(error);
+    }
+  }
+);
+
+export const toFormData = axios.toFormData;
--- src/components/shared/form/customAuth.js
+++ src/components/shared/form/customAuth.js
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { loginAdminService } from "../../../api/services/auth";
 import * as yup from "yup";
+import Cookies from "react-cookie";
 
 function useAuth() {
   const navigate = useNavigate();
@@ -17,7 +18,9 @@ function useAuth() {
     try {
       const res = await loginAdminService(adminInfo);
       if (res.status === 200) {
+        // Cookies.set("token", res.data.accessToken);
         localStorage.setItem("token", res.data.accessToken);
+        localStorage.setItem("refresh_token", res.data.refreshToken);
         navigate("/admin/orders");
       }
     } catch {

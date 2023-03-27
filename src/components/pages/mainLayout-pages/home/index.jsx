65f80b8bdd28f3b13bc9a6abcb29cdf47adb7fee
Minoo <minoo.hadi2904@gmail.com>
2023-03-27 16:51:56 +0400

    feat: add categories on home page

--- src/components/pages/mainLayout-pages/home/index.jsx
+++ src/components/pages/mainLayout-pages/home/index.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCategories } from "../../../../redux/Slices/categoriesSlice";
 
 function Home() {
-    return(
-        <h1>گروه محصولات</h1>
-    )
+  const dispatch = useDispatch();
+  const { categories } = useSelector((store) => store);
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
+  return (
+    <div className="flex flex-col gap-8 px-10 mt-20">
+      {categories.data.map((item) => (
+        <div className="bg-slate-300 flex justify-center items-center h-10 vazir-extraBold text-lg text-slate-700 rounded-xl">
+          {item.name}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Home;

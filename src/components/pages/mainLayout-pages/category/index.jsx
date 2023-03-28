66efa5577ede9052f7daa1ddf8f72df0d57603e8
Minoo <minoo.hadi2904@gmail.com>
2023-03-28 16:27:18 +0400

    feat: go to subcategory page

--- src/components/pages/mainLayout-pages/category/index.jsx
+++ src/components/pages/mainLayout-pages/category/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { baseURL } from "../../../../api/constant";
 import { fetchMainCategories } from "../../../../redux/MainSlices/mainCategorySlice";
 import { fetchSubcategories } from "../../../../redux/Slices/subcategoriesSlice";
@@ -11,6 +11,7 @@ function Category() {
   const dispatch = useDispatch();
   const [params, setParams] = useSearchParams();
   const { mainCategories, subcategories } = useSelector((store) => store);
+  const navigate = useNavigate()
 
   useEffect(() => {
     dispatch(
@@ -20,12 +21,20 @@ function Category() {
     );
   }, [dispatch, params]);
 
+  function goToSubcategoryPage(e) {
+    navigate(`/subcategory?id=${e.target.id}`);
+  }
+
   return (
     <div>
       {Object.entries(mainCategories.data).map((item) => {
         return (
           <div className="flex flex-col px-10 my-16">
-            <div className="bg-slate-300 flex justify-center items-center h-10 vazir-extraBold text-lg text-slate-700 rounded-xl">
+            <div
+              className="bg-slate-300 flex justify-center items-center h-10 vazir-extraBold text-lg text-slate-700 rounded-xl"
+              onClick={goToSubcategoryPage}
+              id={item[0]}
+            >
               {item[1] && item[1][0].subcategory.name}
             </div>
             <div className="flex gap-28 mt-8 flex-wrap justify-center">

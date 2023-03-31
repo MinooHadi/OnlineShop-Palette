ae4b06450193a2577494d2ec6e439200f193be5b
Minoo <minoo.hadi2904@gmail.com>
2023-03-31 23:05:07 +0400

    feat: add toast for admin login

--- src/components/shared/form/customAuth.js
+++ src/components/shared/form/customAuth.js
@@ -4,9 +4,11 @@ import { useNavigate } from "react-router-dom";
 import { loginAdminService } from "../../../api/services/auth";
 import * as yup from "yup";
 import Cookies from "react-cookie";
+import { useState } from "react";
 
 function useAuth() {
   const navigate = useNavigate();
+  const [showLToast, setShowLToast] = useState(undefined)
 
   const loginSchema = yup.object({
     username: yup.string().required("پر کردن این فیلد الزامی می باشد"),
@@ -18,13 +20,15 @@ function useAuth() {
     try {
       const res = await loginAdminService(adminInfo);
       if (res.status === 200) {
-        // Cookies.set("token", res.data.accessToken);
         localStorage.setItem("token", res.data.accessToken);
         localStorage.setItem("refresh_token", res.data.refreshToken);
         navigate("/admin/orders");
       }
     } catch {
-      alert("اطلاعات صحیح نمی باشد");
+      setShowLToast(false);
+      setTimeout(() => {
+        setShowLToast(undefined)
+      }, 2000)
     }
   }
 
@@ -39,6 +43,7 @@ function useAuth() {
     handleSubmit,
     errors,
     login,
+    showLToast
   };
 }
 
--- src/components/shared/form/index.jsx
+++ src/components/shared/form/index.jsx
@@ -5,40 +5,46 @@ import Input from "../input";
 import useAuth from "./customAuth";
 
 import { OutlineArrowLeftIcon } from "../../icons";
+import Toast from "../toast";
 
 function Form() {
-  const { register, handleSubmit, errors, login } = useAuth();
+  const { register, handleSubmit, errors, login, showLToast } = useAuth();
 
   return (
-    <form
-      onSubmit={handleSubmit(login)}
-      className="w-1/3 border-4 rounded-lg border-l-fuchsia-300 border-r-fuchsia-300 border-t-fuchsia-400 border-b-fuchsia-200 py-10 m-auto fixed top-[150px] right-1/3 flex flex-col items-center gap-8"
-    >
-      <div className="self-end pl-12 hover:text-fuchsia-500 text-slate-600">
-        <Link to="/">
-          <OutlineArrowLeftIcon size="1.5rem" />
-        </Link>
-      </div>
-      <Input
-        lable="نام کاربری"
-        type="text"
-        className="border-2 w-96 h-8"
-        validation={{ ...register("username") }}
-        error={errors.username?.message}
-      />
-      <Input
-        lable="رمز عبور"
-        type="password"
-        className="border-2 w-96 h-8"
-        validation={{ ...register("password") }}
-        error={errors.password?.message}
-      />
-      <Input
-        type="submit"
-        value="ورود به پنل ادمین"
-        className="mainHeaderColor w-44 h-14 text-slate-600 font-semibold rounded-full mt-16 vazir-bold"
-      />
-    </form>
+    <>
+      <form
+        onSubmit={handleSubmit(login)}
+        className="w-1/3 border-4 rounded-lg border-l-fuchsia-300 border-r-fuchsia-300 border-t-fuchsia-400 border-b-fuchsia-200 py-10 m-auto fixed top-[150px] right-1/3 flex flex-col items-center gap-8"
+      >
+        <div className="self-end pl-12 hover:text-fuchsia-500 text-slate-600">
+          <Link to="/">
+            <OutlineArrowLeftIcon size="1.5rem" />
+          </Link>
+        </div>
+        <Input
+          lable="نام کاربری"
+          type="text"
+          className="border-2 w-96 h-8"
+          validation={{ ...register("username") }}
+          error={errors.username?.message}
+        />
+        <Input
+          lable="رمز عبور"
+          type="password"
+          className="border-2 w-96 h-8"
+          validation={{ ...register("password") }}
+          error={errors.password?.message}
+        />
+        <Input
+          type="submit"
+          value="ورود به پنل ادمین"
+          className="mainHeaderColor w-44 h-14 text-slate-600 font-semibold rounded-full mt-16 vazir-bold"
+        />
+      </form>
+      {showLToast === false ? (
+        <Toast msg="اطلاعات صحیح نمی باشد" />
+      ) : null}
+    </>
   );
 }
 

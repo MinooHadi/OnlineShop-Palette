bc570278a83090c20c24a7271902d36498ddea56
Minoo <minoo.hadi2904@gmail.com>
2023-04-03 18:54:53 +0400

    feat: set showCFormModal state

--- src/components/pages/mainLayout-pages/card/index.jsx
+++ src/components/pages/mainLayout-pages/card/index.jsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, ShoppingCard } from "../../../shared";
 
 function Card() {
+  const [showCFormModal, setShowCFormModal] = useState(false);
+
+  function showCustomerFormModal() {
+    setShowCFormModal(true);
+  }
+
   return (
-    <div className="p-6 m-10 flex flex-col gap-3 items-center">
-      <ShoppingCard />
-      <ShoppingCard />
-      <ShoppingCard />
-      <ShoppingCard />
-      <div className="w-1/2 flex flex-col gap-4">
-        <div className="flex justify-between items-center w-[100%] vazir-semiBold text-slate-700">
-          <p className="text-lg">جمع مبلغ خرید :</p>
-          <p className="text-sm text-slate-700"> مبلغ به تومان</p>
+    <>
+      <div className="p-6 m-10 flex flex-col gap-3 items-center">
+        <ShoppingCard />
+        <ShoppingCard />
+        <ShoppingCard />
+        <ShoppingCard />
+        <div className="w-1/2 flex flex-col gap-4">
+          <div className="flex justify-between items-center w-[100%] vazir-semiBold text-slate-700">
+            <p className="text-lg">جمع مبلغ خرید :</p>
+            <p className="text-sm text-slate-700"> مبلغ به تومان</p>
+          </div>
+          <Button
+            title="نهایی کردن خرید"
+            className="w-[30%] self-end bg-slate-700 py-3 rounded-lg text-slate-100 vazir-bold"
+            onClick={showCustomerFormModal}
+          />
         </div>
-        <Button title="نهایی کردن خرید" className="w-[30%] self-end bg-slate-700 py-3 rounded-lg text-slate-100 vazir-bold" />
       </div>
-    </div>
+    </>
   );
 }
 

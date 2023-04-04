5b0fd4eee16b463038333a09fac0261c23424ba8
Minoo <minoo.hadi2904@gmail.com>
2023-04-04 19:05:10 +0400

    feat: handle total price

--- src/components/pages/mainLayout-pages/card/index.jsx
+++ src/components/pages/mainLayout-pages/card/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Button, CustomerFormModal, ShoppingCard } from "../../../shared";
 import { useSelector } from "react-redux";
 import { mainProductDetailService } from "../../../../api/mainServices/products";
@@ -8,6 +8,7 @@ function Card() {
   const [showCFormModal, setShowCFormModal] = useState(false);
   const { shoppingCard } = useSelector((store) => store);
   const [data, setData] = useState([]);
+  let totalPrice = 0;
 
   function showCustomerFormModal() {
     setShowCFormModal(true);
@@ -24,41 +25,49 @@ function Card() {
     getProducts();
   }, []);
 
-
   return (
     <>
-    {Object.keys(shoppingCard.cardState).length ? <div className="p-6 m-10 flex flex-col gap-3 items-center">
-        {data.map((item) => {
-          const product = item.data[0];
-          if (shoppingCard.cardState[product.id]) {
-            return (
-              <ShoppingCard
-                name={product.name}
-                price={product.price}
-                thumbnail={product.thumbnail}
-                count={shoppingCard.cardState[product.id]}
-                id={product.id}
-                quantity={product.quantity}
-              />
-            );
-          }
-        })}
-        <div className="w-1/2 flex flex-col gap-4">
-          <div className="flex justify-between items-center w-[100%] vazir-semiBold text-slate-700">
-            <p className="text-lg">جمع مبلغ خرید :</p>
-            <p className="text-sm text-slate-700"> مبلغ به تومان</p>
+      {Object.keys(shoppingCard.cardState).length ? (
+        <div className="p-6 m-10 flex flex-col gap-3 items-center">
+          {data.map((item) => {
+            const product = item.data[0];
+            if (shoppingCard.cardState[product.id]) {
+              console.log(shoppingCard.cardState[product.id]);
+              console.log(product.price);
+              totalPrice += (+shoppingCard.cardState[product.id] * +product.price)
+              return (
+                <ShoppingCard
+                  name={product.name}
+                  price={product.price}
+                  thumbnail={product.thumbnail}
+                  count={shoppingCard.cardState[product.id]}
+                  id={product.id}
+                  quantity={product.quantity}
+                />
+              );
+            }
+          })}
+          <div className="w-1/2 flex flex-col gap-4">
+            <div className="flex justify-between items-center w-[100%] vazir-semiBold text-slate-700">
+              <p className="text-lg">جمع مبلغ خرید :</p>
+              <p className="text-sm text-slate-700"> {totalPrice} تومان </p>
+            </div>
+            <Button
+              title="نهایی کردن خرید"
+              className="w-[30%] self-end bg-slate-700 py-3 rounded-lg text-slate-100 vazir-bold"
+              onClick={showCustomerFormModal}
+            />
           </div>
-          <Button
-            title="نهایی کردن خرید"
-            className="w-[30%] self-end bg-slate-700 py-3 rounded-lg text-slate-100 vazir-bold"
-            onClick={showCustomerFormModal}
-          />
         </div>
-      </div> : <div className="flex flex-col items-center p-36 gap-4">
-        <Basket size="9rem" color="gray" />
-        <p className="vazir-bold text-slate-600">هنوز محصولی به سبد خرید اضافه نکرده اید</p>
-        </div>}
-      
+      ) : (
+        <div className="flex flex-col items-center p-36 gap-4">
+          <Basket size="9rem" color="gray" />
+          <p className="vazir-bold text-slate-600">
+            هنوز محصولی به سبد خرید اضافه نکرده اید
+          </p>
+        </div>
+      )}
+
       {showCFormModal && <CustomerFormModal />}
     </>
   );
